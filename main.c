acb324dd88eff89515e323cf4876ae4a7707bfc2
reloc <reloc@qq.com>
2016-04-26 13:20:36 +0000

    add *******

--- main.c
+++ main.c
@@ -8,7 +8,9 @@ int main(int argc, char *argv[])
 	{
 		printf("%d\n",i);
 	}
-	puts("respberryPi");
+	puts("**********************");
 	puts("git...");
+	puts("**********************");
+
 	return 0;
 }
